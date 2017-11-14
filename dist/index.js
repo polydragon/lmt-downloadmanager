b4b525ab18a33e0f1ccad44592f75b2f06ca2a7e
polydragon <30879984+polydragon@users.noreply.github.com>
2017-11-14 18:43:28 +0000

    updates

--- dist/index.js
+++ dist/index.js
@@ -120,6 +120,7 @@ class DownloadManager {
         return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
             this._emit('download-started', { uuid: uuid });
             let chunkProgress = new Map();
+            let downloadedChunks = 0;
             this._emit('download-status', { uuid: uuid, status: 'Getting chunks to download...' });
             let urls = yield this._getUrlsFromPlaylist(playlist.video.url).catch(err => {
                 return reject(`Couldn't get chunks to download: ${err}`);
@@ -133,9 +134,13 @@ class DownloadManager {
                 }
                 total = Math.floor((total / urls.length) * 100);
                 this._emit('download-progress', { uuid: uuid, percent: total });
+            }).then((result) => {
+                downloadedChunks++;
+                this._emit('download-status', { uuid: uuid, status: `Downloading chunks [${downloadedChunks}/${urls.length}]` });
+                return result;
             });
-            this._emit('download-status', { uuid: uuid, status: 'Downloading chunks' });
-            pMap(urls, mapper, { concurrency: 4 }).then(result => {
+            this._emit('download-status', { uuid: uuid, status: `Downloading chunks [0/${urls.length}]` });
+            pMap(urls, mapper, { concurrency: (this._appSettings.get('downloads.concurrency') || 4) }).then(result => {
                 this._emit('download-status', { uuid: uuid, status: 'Merging chunks' });
                 let concatStr = '';
                 for (let res of result) {
--- src/index.ts
+++ src/index.ts
@@ -150,6 +150,7 @@ export class DownloadManager {
             this._emit('download-started', { uuid: uuid });
 
             let chunkProgress: Map<string, number> = new Map<string, number>();
+            let downloadedChunks = 0;
 
             this._emit('download-status', { uuid: uuid, status: 'Getting chunks to download...' });
 
@@ -167,9 +168,13 @@ export class DownloadManager {
 
                 total = Math.floor((total / (<string[]>urls).length) * 100);
                 this._emit('download-progress', { uuid: uuid, percent: total });
+            }).then((result) => {
+                downloadedChunks++;
+                this._emit('download-status', { uuid: uuid, status: `Downloading chunks [${downloadedChunks}/${(<string[]>urls).length}]` });
+                return result;
             });
 
-            this._emit('download-status', { uuid: uuid, status: 'Downloading chunks' });
+            this._emit('download-status', { uuid: uuid, status: `Downloading chunks [0/${(<string[]>urls).length}]` });
 
             pMap(urls, mapper, { concurrency: (this._appSettings.get('downloads.concurrency') || 4) }).then(result => {
                 this._emit('download-status', { uuid: uuid, status: 'Merging chunks' });
